b78352881a803c2007ca674463ea37e65ba28da5
kaineubauer <kai.neubauer1@gmail.com>
2014-05-20 11:35:24 +0200

    initial commit

---
+++ Conways_SdL/src/de/nordakademie/Conways_SdL/Start.java
@@ -0,0 +1,8 @@
+package de.nordakademie.Conways_SdL;
+
+public class Start {
+
+    public static void main(String[] args) {
+	System.out.println("blabla");
+    }
+}
---
+++ Conways_SdL/src/de/nordakademie/Conways_SdL/tests/StartTest.java
@@ -0,0 +1,11 @@
+package de.nordakademie.Conways_SdL.tests;
+
+import org.junit.Test;
+
+public class StartTest {
+
+    @Test
+    public void test() {
+    }
+
+}
