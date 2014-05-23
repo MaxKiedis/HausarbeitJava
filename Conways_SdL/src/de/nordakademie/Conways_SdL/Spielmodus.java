5789e35d57b5da01150051220d872ce94ea4541a
MaxKiedis <max.anthony@gmx.de>
2014-05-24 01:26:38 +0200

    Update Spielmodus.java

--- Conways_SdL/src/de/nordakademie/Conways_SdL/Spielmodus.java
+++ Conways_SdL/src/de/nordakademie/Conways_SdL/Spielmodus.java
@@ -1,6 +1,7 @@
 package de.nordakademie.Conways_SdL;
 
 /**
+ * Vererbende Klasse der verschiedenen Spielmodi.
  * 
  * @author Max Anthony
  * 
@@ -8,10 +9,11 @@ package de.nordakademie.Conways_SdL;
 public abstract class Spielmodus {
 
     /**
+     * Ermittelt abhaengig vom Spielmodus den Lebenszustand eines Feldes in der naechsten Generation.
      * 
-     * @param nachbaranzahl
-     * @param derzeitigenLebenstand
-     * @return
+     * @param nachbaranzahl Anzahl lebendiger Nachbarn je Feld
+     * @param derzeitigenLebenstand Status des Feldes
+     * @return Zustand des Feldes in naechster Generation
      */
     public abstract boolean gibLebenszustandNaechsteRunde(
 	    final int nachbaranzahl, final boolean derzeitigenLebenstand);
